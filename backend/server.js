417eecc6adfa88afac955b849cd747527e43d735
Harshit Gupta <harshitgupta0910@gmail.com>
2025-07-06 18:03:44 +0530

    connection

--- backend/server.js
+++ backend/server.js
@@ -277,7 +277,7 @@ if (process.env.NODE_ENV === "production") {
   const frontendPath = path.join(__dirname, "../frontend/dist");
   app.use(express.static(frontendPath));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
+    res.sendFile(path.join(frontendPath,"frontend","dist", "index.html"));
   });
 }
 

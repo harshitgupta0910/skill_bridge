891f8d226d842d904bffd6b7bbf5899efec63aea
Himanshu Dubey <arjund030@gmail.com>
2025-07-13 00:32:48 +0530

    fixed minor issue in server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,8 @@ const app = express();
 const server = http.createServer(app);
 
 //  Use your frontend URL here:
-const FRONTEND_URL = "https://skill-bridge-frontend.onrender.com";
+const FRONTEND_URL = "http://localhost:5173";
+// "https://skill-bridge-frontend.onrender.com";
 
 const io = new Server(server, {
   cors: {
